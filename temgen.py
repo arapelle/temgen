acb320608e90db16c7119f873bc2305b8ee154f7
Aymeric Pellé <aympelle@gmail.com>
2024-03-17 20:49:37 +0100

    v0.5.0:#51: Add functions to get system specific template roots.
    linux_template_roots()
    strict_windows_template_roots()
    windows_template_roots()

--- temgen.py
+++ temgen.py
@@ -26,29 +26,42 @@ def environment_template_roots():
     return roots
 
 
-def system_template_roots():
+def linux_template_roots():
     roots = []
+    home_dpath = os.environ['HOME']
+    templates_dpath = Path(f"{home_dpath}/.local/share/{constants.LOWER_PROGRAM_NAME}/templates")
+    templates_dpath.mkdir(parents=True, exist_ok=True)
+    roots.append(templates_dpath)
+    return roots
+
+
+def strict_windows_template_roots():
+    roots = []
+    local_app_data_dpath = Path(os.environ['LOCALAPPDATA'])
+    templates_dpath = local_app_data_dpath / f"{constants.LOWER_PROGRAM_NAME}/templates"
+    templates_dpath.mkdir(parents=True, exist_ok=True)
+    roots.append(templates_dpath)
+    return roots
+
+
+def windows_template_roots():
+    roots = []
+    roots.extend(strict_windows_template_roots())
+    msystem_env_var = os.environ.get('MSYSTEM', None)
+    if msystem_env_var == 'MINGW64' or msystem_env_var == 'MINGW32':
+        roots.extend(linux_template_roots())
+    return roots
+
+
+def system_template_roots():
     platform_system = platform.system().strip().lower()
     match platform_system:
         case "windows":
-            local_app_data_dpath = Path(os.environ['LOCALAPPDATA'])
-            templates_dpath = local_app_data_dpath / f"{constants.LOWER_PROGRAM_NAME}/templates"
-            templates_dpath.mkdir(parents=True, exist_ok=True)
-            roots.append(templates_dpath)
-            msystem_env_var = os.environ.get('MSYSTEM', None)
-            if msystem_env_var == 'MINGW64' or msystem_env_var == 'MINGW32':
-                home_dpath = os.environ['HOME']
-                templates_dpath = Path(f"{home_dpath}/.local/share/{constants.LOWER_PROGRAM_NAME}/templates")
-                templates_dpath.mkdir(parents=True, exist_ok=True)
-                roots.append(templates_dpath)
+            return windows_template_roots()
         case "linux":
-            home_dpath = os.environ['HOME']
-            templates_dpath = Path(f"{home_dpath}/.local/share/{constants.LOWER_PROGRAM_NAME}/templates")
-            templates_dpath.mkdir(parents=True, exist_ok=True)
-            roots.append(templates_dpath)
+            return linux_template_roots()
         case _:
             raise Exception(f"System not handled: '{platform_system}'")
-    return roots
 
 
 def global_template_roots():
