9e00ca5e0a8022dacbf21aeb2ccc838ff4b9a265
Aymeric Pellé <aympelle@gmail.com>
2024-03-20 23:23:26 +0100

    v0.5.0:#46: Add a method in TestTemgenBase to run a test template xml string and retrieve the output file contents.

--- tests/test_temgen_base.py
+++ tests/test_temgen_base.py
@@ -27,3 +27,26 @@ class TestTemgenBase(DirCmpTestCase):
         template_generator = Temgen(TerminalUi())
         template_generator.treat_template_xml_string(template_string, output_dir=Path(self._output_dirpath))
         self.fail()
+
+    def _run__treat_template_xml_string__file_contents__ok(self,
+                                                           file_contents: str,
+                                                           project_root_dir: str,
+                                                           input_parameters):
+        template_string = f"""<?xml version="1.0"?>
+<template>
+    <vars>
+        <var name="project_root_dir" type="gstr" regex="[a-zA-Z0-9_]+" />
+    </vars>
+    <dir path="{project_root_dir}">
+        <file path="data.txt">
+{file_contents}
+        </file>
+    </dir>
+</template>
+        """
+        input_parameters_str = "\n".join(input_parameters)
+        sys.stdin = io.StringIO(f"{project_root_dir}\n{input_parameters_str}")
+        template_generator = Temgen(TerminalUi())
+        template_generator.treat_template_xml_string(template_string, output_dir=Path(self._output_dirpath))
+        with open(f"{self._output_dirpath}/{project_root_dir}/data.txt") as output_file:
+            return output_file.read().strip()
