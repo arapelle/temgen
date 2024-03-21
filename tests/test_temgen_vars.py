405500bf4d2ed2c4496edb181dd86f214eb3b96c
Aymeric Pellé <aympelle@gmail.com>
2024-03-21 01:31:28 +0100

    v0.5.0:#46: Move variable reading tests to TestTemgenVars.
    Remove tests of vars statement with TemgenProgram.

--- tests/test_temgen_vars.py
+++ tests/test_temgen_vars.py
@@ -108,6 +108,151 @@ rand_alpha = '{rand_alpha}'
         input_parameters = []
         self._test__treat_template_xml_string__ok(template_string, project_root_dir, input_parameters)
 
+    @staticmethod
+    def vars__template_string(vars_str: str, file_contents: str):
+        return f"""<?xml version="1.0"?>
+<template>
+    <vars>
+        <var name="output_root_dir" type="gstr" />
+{vars_str}
+    </vars>
+    <dir path="{{output_root_dir}}" >
+        <file path="data.txt">
+{file_contents}
+        </file>
+    </dir>
+</template>
+"""
+
+    @staticmethod
+    def vars_bool__template_string():
+        vars_str = """
+        <var name="first" type="bool" />
+        <var name="second" type="bool" />
+        <var name="third" type="bool" />
+        """
+        file_contents = "bools = {first}, {second}, {third}"
+        return TestTemgenVars.vars__template_string(vars_str, file_contents)
+
+    def test__treat_template_xml_string__vars_bool__y_Y_True__ok(self):
+        template_string = self.vars_bool__template_string()
+        project_root_dir = "template_xml_string__vars_bool__y_Y_True"
+        input_parameters = ["", "false", "no", "y", "Y", "True"]
+        self._test__treat_template_xml_string__ok(template_string, project_root_dir, input_parameters)
+
+    def test__treat_template_xml_string__vars_bool__n_N_False__ok(self):
+        template_string = self.vars_bool__template_string()
+        project_root_dir = "template_xml_string__vars_bool__n_N_False"
+        input_parameters = ["", "true", "yes", "n", "N", "False"]
+        self._test__treat_template_xml_string__ok(template_string, project_root_dir, input_parameters)
+
+    def test__treat_template_xml_string__vars_int__ok(self):
+        vars_str = """
+        <var name="first" type="int" />
+        <var name="second" type="int" />
+        """
+        file_contents = "ints = {first}, {second}"
+        template_string = self.vars__template_string(vars_str, file_contents)
+        project_root_dir = "template_xml_string__vars_int"
+        input_parameters = ["7t", "42.5", "36", "-42.5", "-37"]
+        self._test__treat_template_xml_string__ok(template_string, project_root_dir, input_parameters)
+
+    def test__treat_template_xml_string__vars_uint__ok(self):
+        vars_str = """
+        <var name="first" type="uint" />
+        """
+        file_contents = "uints = {first}"
+        template_string = self.vars__template_string(vars_str, file_contents)
+        project_root_dir = "template_xml_string__vars_uint"
+        input_parameters = ["7t", "42.5", "-42.5", "-37", "36"]
+        self._test__treat_template_xml_string__ok(template_string, project_root_dir, input_parameters)
+
+    def test__treat_template_xml_string__vars_float__ok(self):
+        vars_str = """
+        <var name="first" type="float" />
+        <var name="second" type="float" />
+        <var name="third" type="float" />
+        <var name="fourth" type="float" />
+        <var name="fifth" type="float" />
+        <var name="sixth" type="float" />
+        """
+        file_contents = "floats = {first}, {second}, {third}, {fourth}, {fifth}, {sixth}"
+        template_string = self.vars__template_string(vars_str, file_contents)
+        project_root_dir = "template_xml_string__vars_float"
+        input_parameters = ["7t", "52", "-32", "65.0", "67.2", "-72.0", "-74.3"]
+        self._test__treat_template_xml_string__ok(template_string, project_root_dir, input_parameters)
+
+    def test__treat_template_xml_string__vars_str__ok(self):
+        vars_str = """
+        <var name="first" type="str" />
+        <var name="second" type="str" />
+        <var name="third" type="str" />
+        <var name="fourth" type="str" />
+        """
+        file_contents = "strs = '{first}', '{second}', '{third}', '{fourth}'"
+        template_string = self.vars__template_string(vars_str, file_contents)
+        project_root_dir = "template_xml_string__vars_str"
+        input_parameters = ["", "  ", "info", "  info  "]
+        self._test__treat_template_xml_string__ok(template_string, project_root_dir, input_parameters)
+
+    def test__treat_template_xml_string__vars_pstr__ok(self):
+        vars_str = """
+        <var name="first" type="pstr" />
+        <var name="second" type="pstr" />
+        <var name="third" type="pstr" />
+        """
+        file_contents = "pstrs = '{first}', '{second}', '{third}'"
+        template_string = self.vars__template_string(vars_str, file_contents)
+        project_root_dir = "template_xml_string__vars_pstr"
+        input_parameters = ["", "  ", "info", "  info  "]
+        self._test__treat_template_xml_string__ok(template_string, project_root_dir, input_parameters)
+
+    def test__treat_template_xml_string__vars_gstr__ok(self):
+        vars_str = """
+        <var name="first" type="pstr" />
+        <var name="second" type="pstr" />
+        """
+        file_contents = "gstrs = '{first}', '{second}'"
+        template_string = self.vars__template_string(vars_str, file_contents)
+        project_root_dir = "template_xml_string__vars_gstr"
+        input_parameters = ["", "  ", "info", "  info  "]
+        self._test__treat_template_xml_string__ok(template_string, project_root_dir, input_parameters)
+
+    def test__treat_template_xml_string__vars_bad_type__exception(self):
+        var_type = "conaipa"
+        vars_str = f"""
+        <var name="first" type="{var_type}" />
+        """
+        file_contents = "any = {first}"
+        template_string = self.vars__template_string(vars_str, file_contents)
+        project_root_dir = "template_xml_string__vars_gstr"
+        input_parameters = ["", "  ", "info", "  info  "]
+        try:
+            self._test__treat_template_xml_string__ok(template_string, project_root_dir, input_parameters)
+        except RuntimeError as err:
+            self.assertTrue(str(err).startswith("Bad var_type: '"))
+            self.assertTrue(str(err).find(f"{var_type}") != -1)
+
+    def test__treat_template_xml_string__vars_default__ok(self):
+        vars_str = """
+        <var name="first" type="gstr" default="dummy" />
+        """
+        file_contents = "str = '{first}'"
+        template_string = self.vars__template_string(vars_str, file_contents)
+        project_root_dir = "template_xml_string__vars_default"
+        input_parameters = ["\n"]
+        self._test__treat_template_xml_string__ok(template_string, project_root_dir, input_parameters)
+
+    def test__treat_template_xml_string__vars_regex__ok(self):
+        vars_str = """
+        <var name="first" type="gstr" regex="[A-Z]{4}_[0-9]{2}" />
+        """
+        file_contents = "str = '{first}'"
+        template_string = self.vars__template_string(vars_str, file_contents)
+        project_root_dir = "template_xml_string__vars_regex"
+        input_parameters = ["\n", "bale_26", "BANA23", "AZER_58"]
+        self._test__treat_template_xml_string__ok(template_string, project_root_dir, input_parameters)
+
     def test__treat_template_xml_string__vars_if__ok(self):
         template_string = """<?xml version="1.0"?>
 <template>
